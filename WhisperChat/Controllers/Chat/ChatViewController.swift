d228f5c05b3eb235b68f1a2707a734d2edae2f30
mahmadali3180 <mahmadali3180@gmail.com>
2023-04-17 07:30:07 +0500

    Embed Views in TabBarController and NavigationControllers

--- WhisperChat/Controllers/Chat/ChatViewController.swift
+++ WhisperChat/Controllers/Chat/ChatViewController.swift
@@ -10,37 +10,16 @@ import FirebaseAuth
 
 class ChatViewController: UIViewController {
     
-    //MARK: - Create UI Elemests
-    
-    private let signOutButton: UIButton = {
-        var button = UIButton()
-        button.setTitle("Sign Out", for: .normal)
-        button.layer.cornerRadius = 12
-        button.tintColor = .white
-        button.layer.masksToBounds = true
-        button.layer.borderWidth = 0.5
-        button.layer.borderColor = UIColor.lightGray.cgColor
-        button.backgroundColor = .systemTeal
-        return button
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
-        view.addSubview(signOutButton)
-        signOutButton.addTarget(self, action: #selector(signOutButtonTapped), for: .touchUpInside)
+        view.backgroundColor = .systemGray6
+        
     }
     
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         validateAuth()
-    }
-    
-    override func viewDidLayoutSubviews() {
-        let customFrame = view.bounds
-        
-        signOutButton.frame = CGRect(x: 30, y: view.top + 90, width: customFrame.width-60, height: 52)
     }
     
     private func validateAuth() {
@@ -58,19 +37,6 @@ class ChatViewController: UIViewController {
         let action = UIAlertAction(title: "Ok", style: .cancel)
         alert.addAction(action)
         present(alert, animated: true)
-    }
-    
-    @objc private func signOutButtonTapped() {
-        do {
-            let vc = LoginViewController()
-            let nc = UINavigationController(rootViewController: vc)
-            nc.modalPresentationStyle = .fullScreen
-            try FirebaseAuth.Auth.auth().signOut()
-            present(nc, animated: true)
-            print("Signed out!")
-        } catch {
-            alertUserLoginError(title: "Operation Failed", message: "Can't Signout current user.")
-        }
     }
 }
 
--- WhisperChat/Controllers/Profile/ProfileViewController.swift
+++ WhisperChat/Controllers/Profile/ProfileViewController.swift
@@ -6,12 +6,56 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ProfileViewController: UIViewController {
-
+    
+    //MARK: - Create UI Elemests
+    
+    private let signOutButton: UIButton = {
+        var button = UIButton()
+        button.setTitle("Sign Out", for: .normal)
+        button.layer.cornerRadius = 12
+        button.tintColor = .white
+        button.layer.masksToBounds = true
+        button.layer.borderWidth = 0.5
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.backgroundColor = .systemTeal
+        return button
+    }()
+    
+    override func viewDidLayoutSubviews() {
+        let customFrame = view.bounds
+        
+        signOutButton.frame = CGRect(x: 30, y: (navigationController?.navigationBar.bottom)! + 40, width: customFrame.width-60, height: 52)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //        Adding SubViews
+        view.addSubview(signOutButton)
+        signOutButton.addTarget(self, action: #selector(signOutButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func signOutButtonTapped() {
+        do {
+            let vc = LoginViewController()
+            let nc = UINavigationController(rootViewController: vc)
+            nc.modalPresentationStyle = .fullScreen
+            try FirebaseAuth.Auth.auth().signOut()
+            present(nc, animated: true)
+            print("Signed out!")
+        } catch {
+            alertUserLoginError(title: "Operation Failed", message: "Can't Signout current user.")
+        }
+    }
+    
+    func alertUserLoginError(title: String, message: String = "Error signing out.") {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .cancel)
+        alert.addAction(action)
+        present(alert, animated: true)
     }
     
 }
